11852e460bbcbed8846141c40e838281a8aae1a5
joshua <joshua@localhost.localdomain>
2020-06-05 14:54:53 -0400

    Modified forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,8 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Unable to find location.')
         }else{
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature}. It feels like ${body.current.feelslike}`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature}. It feels like ${body.current.feelslike}. 
+            The humidity is ${body.current.humidity}%. With a wind speed of ${body.current.wind_speed}mph.`)
         }
     })
 }
